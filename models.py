6d668c7c5b6d53b865a1d62a08fcb15a41841c84
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-05 17:49:17 +0100

    invoke uuid for id field

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from uuid import UUID, uuid4
 from pydantic import BaseModel
 
 class Article(BaseModel):
-  id: Optional[UUID] = uuid4
+  id: Optional[UUID] = uuid4()
   author: Optional[str] = str
   post_date: Optional[str] = str
   post_time: Optional[str] = str
