b10582118dfd7daef6206e18403526c445be697b
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-07 17:56:26 +0100

    rename object keys

--- scraper.py
+++ scraper.py
@@ -18,12 +18,11 @@ def get_sport24_article(uri):
   content = driver.find_element(By.CSS_SELECTOR, '.article-single')
 
   return {
-    "website": "sport24",
     "author": content.find_element(By.CSS_SELECTOR, '.article-single__byline span').text,
-    "post_date": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[0].strip(),
-    "post_time": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[-1].strip(),
-    "img_link": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[1].strip().split(' ')[0],
-    "small_img_link": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[0].split(' ')[0],
+    "date": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[0].strip(),
+    "time": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[-1].strip(),
+    "img": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[1].strip().split(' ')[0],
+    "thumbnail": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[0].split(' ')[0],
     "title": content.find_element(By.CSS_SELECTOR, '.article-single__title').text.strip(),
     "body": [{tag.tag_name:tag.text} for tag in content.find_elements(By.CSS_SELECTOR, '.article-single__body > h2,p')]
   }
