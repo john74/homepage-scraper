ac9f46933544893cd177c07ff2091d3f13d3a972
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-07 13:28:05 +0100

    refactor get_sport24_greek_sports_articles(). The function now returns a single object. rename function to get_sport24_article

--- scraper.py
+++ scraper.py
@@ -11,35 +11,21 @@ option = webdriver.ChromeOptions()
 option.add_argument('headless')
 driver = webdriver.Chrome(service=service, options=option)
 
-teams = ['panathinaikos', 'olympiacos', 'aek', 'paok', 'aris']
-def get_sport24_greek_sports_articles(teams):
-  articles = []
-  titles = []
-  for team in teams:
-    url = f'https://www.sport24.gr/football/{team}'
-    driver.get(url)
-    preview_content = driver.find_element(By.CSS_SELECTOR, '.teaser__content > :first-child')
-    link = preview_content.find_element(By.CSS_SELECTOR, '.article__image > a').get_attribute('href').strip()
-    driver.get(link)
-    content = driver.find_element(By.CSS_SELECTOR, '.article-single')
-    article_title = content.find_element(By.CSS_SELECTOR, '.article-single__title').text.strip()
-    if article_title in titles: continue
-    titles.append(article_title)
-    article_body = [{tag.tag_name:tag.text} for tag in content.find_elements(By.CSS_SELECTOR, '.article-single__body > h2,p')]
+def get_sport24_article(uri):
+  driver.get(f'https://www.sport24.gr/{uri}')
+  url = driver.find_element(By.CSS_SELECTOR, '.teaser__content > :first-child .article__image > a').get_attribute('href').strip()
+  driver.get(url)
+  content = driver.find_element(By.CSS_SELECTOR, '.article-single')
 
-    articles.append({
-      "website": "sport24",
-      "author": content.find_element(By.CSS_SELECTOR, '.article-single__byline__author').text.strip(),
-      "post_date": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[0].strip(),
-      "post_time": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[-1].strip(),
-      "article_img_link": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[1].strip().split(' ')[0],
-      "article_small_img_link": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[0].split(' ')[0],
-      "article_title": article_title,
-      "article_body": article_body
-    })
+  return {
+    "website": "sport24",
+    "author": content.find_element(By.CSS_SELECTOR, '.article-single__byline__author').text.strip(),
+    "post_date": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[0].strip(),
+    "post_time": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[-1].strip(),
+    "article_img_link": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[1].strip().split(' ')[0],
+    "article_small_img_link": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[0].split(' ')[0],
+    "article_title": content.find_element(By.CSS_SELECTOR, '.article-single__title').text.strip(),
+    "article_body": [{tag.tag_name:tag.text} for tag in content.find_elements(By.CSS_SELECTOR, '.article-single__body > h2,p')]
+  }
 
-  driver.quit()
-  return articles
-
-def get_sport24_team_articles(teams):
-  pass
+driver.quit()
