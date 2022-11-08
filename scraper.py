5b5f7b3d9dde7b67f8ba09a5ffbeab0769665de0
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-08 12:03:09 +0100

    fix indentation to four tab spaces.remove list comprehensions from return statements in favor of for loops to make code more clear.create get_sport24_greek_competitions()

--- scraper.py
+++ scraper.py
@@ -11,30 +11,44 @@ option = webdriver.ChromeOptions()
 option.add_argument('headless')
 driver = webdriver.Chrome(service=service, options=option)
 
+def get_sport24_greek_competitions():
+    driver.get('https://www.sport24.gr')
+    competitions = driver.find_elements(By.CSS_SELECTOR, '.main-menu__side-panel-container > :nth-child(3) > .main-menu__dropdown main-menu__dropdown--mega-menu [itemprop="url"')
+
 def get_sport24_greek_team_names():
-  driver.get('https://www.sport24.gr')
-  teams = driver.find_elements(By.CSS_SELECTOR, '.main-menu__group--teams_football_greece [itemprop="url"')
-  return [team.get_attribute('href').split('/')[-1].strip() for team in teams]
+    driver.get('https://www.sport24.gr')
+    teams = driver.find_elements(By.CSS_SELECTOR, '.main-menu__group--teams_football_greece [itemprop="url"')
+    names = []
+    for team in teams:
+        href = team.get_attribute('href')
+        name = href.split('/')[-1]
+        names.append(name.strip())
+    return names
 
 def get_sport24_international_team_names():
-  driver.get('https://www.sport24.gr')
-  teams = driver.find_elements(By.CSS_SELECTOR, '.main-menu__group--teams_football_international [itemprop="url"')
-  return [team.get_attribute('href').split('/')[-1].strip() for team in teams]
+    driver.get('https://www.sport24.gr')
+    teams = driver.find_elements(By.CSS_SELECTOR, '.main-menu__group--teams_football_international [itemprop="url"')
+    names = []
+    for team in teams:
+        href = team.get_attribute('href')
+        name = href.split('/')[-1]
+        names.append(name.strip())
+    return names
 
 def get_sport24_article(uri):
-  driver.get(f'https://www.sport24.gr/{uri}')
-  url = driver.find_element(By.CSS_SELECTOR, '.teaser__content > :first-child .article__image > a').get_attribute('href').strip()
-  driver.get(url)
-  content = driver.find_element(By.CSS_SELECTOR, '.article-single')
+    driver.get(f'https://www.sport24.gr/{uri}')
+    url = driver.find_element(By.CSS_SELECTOR, '.teaser__content > :first-child .article__image > a').get_attribute('href').strip()
+    driver.get(url)
+    content = driver.find_element(By.CSS_SELECTOR, '.article-single')
 
-  return {
-    "author": content.find_element(By.CSS_SELECTOR, '.article-single__byline span').text,
-    "date": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[0].strip(),
-    "time": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[-1].strip(),
-    "img": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[1].strip().split(' ')[0],
-    "thumbnail": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[0].split(' ')[0],
-    "title": content.find_element(By.CSS_SELECTOR, '.article-single__title').text.strip(),
-    "body": [{tag.tag_name:tag.text} for tag in content.find_elements(By.CSS_SELECTOR, '.article-single__body > h2,p')]
-  }
+    return {
+        "author": content.find_element(By.CSS_SELECTOR, '.article-single__byline span').text,
+        "date": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[0].strip(),
+        "time": content.find_element(By.CSS_SELECTOR, '.article-single__byline > time').text.split(' ')[-1].strip(),
+        "img": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[1].strip().split(' ')[0],
+        "thumbnail": content.find_element(By.CSS_SELECTOR, '.article-single__image--main > img').get_attribute('srcset').split(',')[0].split(' ')[0],
+        "title": content.find_element(By.CSS_SELECTOR, '.article-single__title').text.strip(),
+        "body": [{tag.tag_name:tag.text} for tag in content.find_elements(By.CSS_SELECTOR, '.article-single__body > h2,p')]
+    }
 
 # driver.quit()
