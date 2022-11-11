b3f80b563c21f101da48fd5f9d6f3e2b1e840540
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-11 18:28:02 +0100

    group variables to dictionaries

--- sport24/variables.py
+++ sport24/variables.py
@@ -1,28 +1,30 @@
 """
 The urls and the selectors for all operations in sport24 website
 """
+QUERY_PARAMETERS = {
+    # // FOOTBALL
+    'greek_football': 'nav > div > :nth-child(3) > div > :nth-child(1) a',
+    'international_football': 'nav > div > :nth-child(3) > div > :nth-child(2) a',
+    'greek_football_teams': '[class*="teams_football_greece"] a',
+    'international_football_teams': '[class*="teams_football_international"] a',
 
-# // WEBSITE
-BASE_URL = 'https://www.sport24.gr/'
-DATE_FORMAT = '%d.%m.%Y'
+    # // BASKETBALL
+    'greek_basketball_teams': '[class*="teams_basketball_greece"] a',
+    'euroleague_teams': '[class*="teams_basketball_euroleague"] a',
+    'nba_teams': '[class*="teams_basketball_nba"] a'
+}
 
-# // FOOTBALL
-GREEK_FOOTBALL_SELECTOR = '.main-menu__side-panel-container > :nth-child(3) > div > :first-child [itemprop="url"]'
-INTERNATIONAL_FOOTBALL_SELECTOR = '.main-menu__side-panel-container > :nth-child(3) > div > :nth-child(2) [itemprop="url"]'
-GREEK_FOOTBALL_TEAMS_SELECTOR = '[class*="teams_football_greece"] [itemprop="url"]'
-INTERNATIONAL_FOOTBALL_TEAMS_SELECTOR = '[class*="teams_football_international"] [itemprop="url"]'
+WEBSITE = {
+    'url': 'https://www.sport24.gr/',
+    'date_format': '%d.%m.%Y'
+}
 
-# // BASKETBALL
-GREEK_BASKETBALL_TEAMS_SELECTOR = '[class*="teams_basketball_greece"] [itemprop="url"]'
-EUROLEAGUE_TEAMS_SELECTOR = '[class*="teams_basketball_euroleague"] [itemprop="url"]'
-NBA_TEAMS_SELECTOR = '[class*="teams_basketball_nba"] [itemprop="url"]'
-
-# // ARTICLE
-ARTICLE_URL_SELECTOR = '.teaser__content a'
-ARTICLE_CONTENT_SELECTOR = '#article-container'
-AUTHOR_SELECTOR = '.article-single__byline span'
-DATE_SELECTOR = '.article-single__byline > time'
-TIME_SELECTOR = '.article-single__byline > time'
-IMAGE_SELECTOR = '.article-single__image--main > img'
-TITLE_SELECTOR = '.article-single__title'
-BODY_SELECTOR = '.article-single__body > p'
+ARTICLE = {
+    'url': '.article__title a',
+    'title': '.article-single__title',
+    'content': '#article-container',
+    'author': '.article-single__byline span',
+    'datetime': '.article-single__byline > time',
+    'image': 'figure > img',
+    'body': '.article-single__body > p'
+}
