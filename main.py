311a279e918251d5993954c2fa9fadf56287aaeb
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-13 21:22:20 +0100

    import flashscore module.add flashscore route

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
 from sport24 import sport24
-
+from flashscore import flashscore
 
 # run server as uvicorn main:app --port 8086  --reload
 app = FastAPI()
 
 
 app.include_router(sport24.router)
+app.include_router(flashscore.router)
