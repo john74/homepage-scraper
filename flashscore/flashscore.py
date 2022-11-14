d3eef550838090e5af320dcb03d7c60db8308886
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-14 15:42:08 +0100

    change endpoint to /api/football-standings/

--- flashscore/flashscore.py
+++ flashscore/flashscore.py
@@ -4,14 +4,10 @@ from . import scraper
 
 router = APIRouter()
 
-@router.get("/api/sport-standings/")
-def get_sport_standings(sport, country, league):
+@router.get("/api/football-standings/")
+def get_football_standings(country, league):
     """
     docstring
     """
-    # if sport.lower() == 'basketball':
-    #     league = FOOTBALL_LEAGUES[country]
-    # else:
-    #     league = BASKETBALL_LEAGUES[country]
-    league_table = scraper.get_league_table(sport, country, league)
+    league_table = scraper.get_football_league_table(country, league)
     return scraper.get_league_standings(league_table)
