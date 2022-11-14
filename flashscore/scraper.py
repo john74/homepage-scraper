075088f6fe8388992d422b785feac44a41ca04bf
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-14 15:06:01 +0100

    split next_match_data into smaller parts.remove datetime import.matches_played value is calculated by the sum of wins, draws and losses

--- flashscore/scraper.py
+++ flashscore/scraper.py
@@ -1,5 +1,4 @@
 import os, re
-from datetime import datetime, date
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -14,6 +13,9 @@ option.add_argument('headless')
 driver = webdriver.Chrome(service=service, options=option)
 
 def get_league_table(sport, country, league):
+    """
+    docstring
+    """
     driver.get(f'{WEBSITE["url"]}/{sport}/{country}/{league}/{WEBSITE["standings"]}/')
     return driver.find_elements(By.CSS_SELECTOR, '.ui-table__body > div')
 
@@ -28,14 +30,17 @@ def get_league_standings(league_table):
         rank = index
         name = team.find_element(By.CSS_SELECTOR, TEAM['name']).text
         status = team.find_element(By.CSS_SELECTOR, TEAM['status']).get_attribute('title')
-        matches_played = team.find_element(By.CSS_SELECTOR, TEAM['matches_played']).text
         wins = team.find_element(By.CSS_SELECTOR, TEAM['wins']).text
         draws = team.find_element(By.CSS_SELECTOR, TEAM['draws']).text
         losses = team.find_element(By.CSS_SELECTOR, TEAM['losses']).text
+        matches_played = int(wins) + int(draws) + int(losses)
         points = team.find_element(By.CSS_SELECTOR, TEAM['points']).text
-        next_match_data = team.find_element(By.CSS_SELECTOR, TEAM['next_match_data']).get_attribute('title').split('\n')
-        next_match_opponents = next_match_data[1]
-        next_match_date = next_match_data[-1]
+        next_match_data = team.find_element(By.CSS_SELECTOR, TEAM['next_match_data'])
+        next_match_data_list = next_match_data.get_attribute('title').split('\n')
+        next_match_opponents_list = next_match_data_list[1].split(' - ')
+        next_match_home_team = next_match_opponents_list[0]
+        next_match_away_team = next_match_opponents_list[-1]
+        next_match_date = next_match_data_list[-1]
         last_five_results = team.find_elements(By.CSS_SELECTOR, TEAM['last_five_results'])
 
         previous_results = []
@@ -56,12 +61,13 @@ def get_league_standings(league_table):
             "rank": rank,
             "name": name.strip(),
             'status': status.strip(),
-            'matches_played': matches_played.strip(),
+            'matches_played': str(matches_played).strip(),
             'wins': wins.strip(),
             'draws': draws.strip(),
             'losses': losses.strip(),
             'points': points.strip(),
-            'next_match_opponents': next_match_opponents.strip(),
+            'next_match_home_team': next_match_home_team.strip(),
+            'next_match_away_team': next_match_away_team.strip(),
             'next_match_date': next_match_date.strip(),
             'previous_results': previous_results
         })
