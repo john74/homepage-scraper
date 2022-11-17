5c9a9ee5b7a3a20101200d91e5a60426ba6537f0
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-17 14:57:36 +0100

    rename get_football_league_table() to get_football_league_tables()

--- standings/standings.py
+++ standings/standings.py
@@ -9,5 +9,5 @@ def get_football_standings(country, league):
     """
     docstring
     """
-    league_table = scraper.get_football_league_table(country, league)
+    league_table = scraper.get_football_league_tables(country, league)
     return scraper.get_league_standings(league_table)
