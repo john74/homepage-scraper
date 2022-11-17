deb94e023c68fd2aba39ee95e981bbfa99c3e5e0
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-17 15:58:37 +0100

    add proper docstring

--- standings/standings.py
+++ standings/standings.py
@@ -7,7 +7,9 @@ router = APIRouter()
 @router.get("/api/football-standings/")
 def get_football_standings(country, league):
     """
-    docstring
+    Returns the standings of all the football teams
+    that compete at the league specified by the country
+    and league parameters
     """
     league_table = scraper.get_football_league_tables(country, league)
     return scraper.get_league_standings(league_table)
