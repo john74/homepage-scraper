f7a54b21aa5b394820d35687495481e1f5fa8068
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-17 11:15:39 +0100

    add docstrings.replace regex with variables.rename variables

--- flashscore/scraper.py
+++ flashscore/scraper.py
@@ -14,12 +14,16 @@ driver = webdriver.Chrome(service=service, options=option)
 
 def get_football_league_table(country, league):
     """
-    docstring
+    Returns the table with the standings for all the football teams
+    of the league
     """
     driver.get(f'{WEBSITE["url"]}/football/{country}/{league}/{WEBSITE["standings"]}/')
     return driver.find_elements(By.CSS_SELECTOR, WEBSITE['league_table'])
 
-def get_team_standings(index, team):
+def get_team_standings(team):
+    """
+    Returns the standings for of a team
+    """
     wins = team.find_element(By.CSS_SELECTOR, TEAM['wins']).text
     draws = team.find_element(By.CSS_SELECTOR, TEAM['draws']).text
     losses = team.find_element(By.CSS_SELECTOR, TEAM['losses']).text
@@ -28,7 +32,6 @@ def get_team_standings(index, team):
     next_match_opponents_list = next_match_data_list[1].split(' - ')
 
     return {
-        "rank": index,
         "name": team.find_element(By.CSS_SELECTOR, TEAM['name']).text,
         "status": team.find_element(By.CSS_SELECTOR, TEAM['status']).get_attribute('title'),
         "wins": wins,
@@ -42,16 +45,19 @@ def get_team_standings(index, team):
     }
 
 def get_recent_results(team):
+    """
+    Returns the most recent past results of a team
+    """
     recent_results = team.find_elements(By.CSS_SELECTOR, TEAM['recent_results'])
     results = []
     for result in recent_results:
         data = result.get_attribute('title')
-        score = re.findall('(\\d+:\\d+)&', data)[0]
-        match_opponents = re.findall('\\((\\w+.+)\\)', data)[0]
-        match_date = re.findall('(\\d+\\..+)$', data)[0]
+        match_score = re.findall(TEAM['match_score'], data)[0]
+        match_opponents = re.findall(TEAM['match_opponents'], data)[0]
+        match_date = re.findall(TEAM['match_date'], data)[0]
         result_letter_indicator = result.text
         results.append([
-            score.strip(),
+            match_score.strip(),
             match_opponents.strip(),
             match_date.strip(),
             result_letter_indicator.strip()
@@ -60,11 +66,12 @@ def get_recent_results(team):
 
 def get_league_standings(league_table):
     """
-    docstring
+    Returns the standings of all the teams of the league table
     """
     league_standings = []
-    for index, team in enumerate(league_table, start=1):
-        team_standings = get_team_standings(index, team)
+    for rank, team in enumerate(league_table, start=1):
+        team_standings = get_team_standings(team)
+        team_standings['rank'] = rank
         team_standings['recent_results'] = get_recent_results(team)
         league_standings.append(team_standings)
     return league_standings
