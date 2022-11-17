e9d9edc5c3bb65ad41942adc6d26e69c21c52508
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-17 11:36:00 +0100

    rename flashscore to standings

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
 from sport24 import sport24
-from flashscore import flashscore
+from standings import standings
 
 # run server as uvicorn main:app --port 8086  --reload
 app = FastAPI()
 
 
 app.include_router(sport24.router)
-app.include_router(flashscore.router)
+app.include_router(standings.router)
