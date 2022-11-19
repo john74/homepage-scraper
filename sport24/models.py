16d293f319c03f1f2bf68226b2d0bc1bc8ef4f13
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-19 11:40:47 +0100

    create sqlalchemy Article model

--- sport24/models.py
+++ sport24/models.py
@@ -1,13 +1,17 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from .database import Base
 
 
-class Article(BaseModel):
-    url: str
-    author: str
-    date: str
-    time: str
-    small_image: str
-    medium_image: str
-    large_image: str
-    title: str
-    body: str
+class Article(Base):
+    __tablename__ = 'articles'
+
+    id = Column(Integer, primary_key=True, index=True)
+    url = Column(String)
+    author = Column(String)
+    date = Column(String)
+    time = Column(String)
+    small_image = Column(String)
+    medium_image = Column(String)
+    large_image = Column(String)
+    title = Column(String)
+    body = Column(String)
