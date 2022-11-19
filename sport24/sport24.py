e6e3aa5f57951d9316b1f81c12dae1f7566af096
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-19 11:41:43 +0100

    connect model with the database

--- sport24/sport24.py
+++ sport24/sport24.py
@@ -1,14 +1,27 @@
 from fastapi import APIRouter
 
-from . import scraper
-from . variables import QUERY_PARAMETERS
+# from . import scraper
+# from . variables import QUERY_PARAMETERS
+from . import schemas, models, database
+
 router = APIRouter()
+models.Base.metadata.create_all(bind=database.engine)
+
+# @router.get("/api/sport24-articles/")
+# async def get_sport24_articles(category):
+#     """
+#     Returns the articles from all football and basketball
+#     navbar categories
+#     """
+#     category_urls = scraper.get_category_urls(QUERY_PARAMETERS[category])
+#     return await scraper.get_articles(category_urls)
 
 @router.get("/api/sport24-articles/")
-async def get_sport24_articles(category):
+async def get_sport24_articles(request: schemas.ArticleBase):
     """
     Returns the articles from all football and basketball
     navbar categories
     """
-    category_urls = scraper.get_category_urls(QUERY_PARAMETERS[category])
-    return await scraper.get_articles(category_urls)
+    return request
+    # category_urls = scraper.get_category_urls(QUERY_PARAMETERS[category])
+    # return await scraper.get_articles(category_urls)
