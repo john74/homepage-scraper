cd5cf11f1f50cab2452d6e5d7f902e98eba596cf
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-20 14:47:46 +0100

    change the path that the sqlite database will be created

--- sport24/database.py
+++ sport24/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///../articles.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///articles.db'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
