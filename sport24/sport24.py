92aeef1869b114037963c03274cdd5ce7c533c6c
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-23 20:41:15 +0100

    add docstrings.create /api/store-sport24-articles endpoint for post method

--- sport24/sport24.py
+++ sport24/sport24.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from .import scraper
+from . import scraper
 from .variables import QUERY_PARAMETERS
-from .import models, database
+from . import models, database
 from .models import Article
 
 
@@ -12,11 +12,21 @@ router = APIRouter()
 # create database tables
 models.Base.metadata.create_all(bind=database.engine)
 
-# dependency
 # https://fastapi.tiangolo.com/tutorial/sql-databases/?h=models.base#create-a-dependency
 def get_db():
+    """
+    Dependency.
+    Creates an independent database session per request,
+    and closes the connection when all the request
+    operations are finished
+    """
     db = database.SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+@router.post("/api/store-sport24-articles")
+def store_articles(category, db: Session = Depends(get_db)):
+    category_selector = QUERY_PARAMETERS[category]
