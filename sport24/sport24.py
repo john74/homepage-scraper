fe09384a01756f21d181b13c793e249cf5d8d456
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-23 18:40:51 +0100

    add comments.create get_db() dependency

--- sport24/sport24.py
+++ sport24/sport24.py
@@ -6,5 +6,17 @@ from .import models, database
 from .models import Article
 
 
+# router path operation
+# https://fastapi.tiangolo.com/tutorial/bigger-applications/?h=apir#import-apirouter
 router = APIRouter()
+# create database tables
 models.Base.metadata.create_all(bind=database.engine)
+
+# dependency
+# https://fastapi.tiangolo.com/tutorial/sql-databases/?h=models.base#create-a-dependency
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
