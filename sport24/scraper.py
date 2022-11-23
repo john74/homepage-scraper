a9ff37d1c6c6f53421f2c9faf0b92fed2a8f56b7
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-23 21:44:30 +0100

    create get_category_name_url_pairs()

--- sport24/scraper.py
+++ sport24/scraper.py
@@ -18,3 +18,17 @@ def get_category_urls(anchor_elements):
         url = anchor.get_attribute('href')
         urls.add(url.strip())
     return urls
+
+
+def get_category_name_url_pairs(category_urls):
+    """
+    Returns the names of the categories and the category urls
+    as key value pairs.
+    key: the part after the last / of the category url.
+    value: the category url.
+    """
+    pairs = {}
+    for url in category_urls:
+        category = url.split('/')[-1]
+        pairs[category] = url
+    return pairs
