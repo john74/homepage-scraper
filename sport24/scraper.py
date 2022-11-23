8920733e6e5e1fa8bc853f3b1f7a907d2324dc8a
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-23 21:00:06 +0100

    add imports.create get_category_urls()

---
+++ sport24/scraper.py
@@ -0,0 +1,20 @@
+import re
+from datetime import datetime, date
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from driver import driver
+from .variables import WEBSITE
+
+
+def get_category_urls(anchor_elements):
+    """
+    Accepts the css selector of all a tags of the category.
+    Returns a set containing the urls from all a tags.
+    """
+    driver.get(WEBSITE['url'])
+    anchor_elements = driver.find_elements(By.CSS_SELECTOR, anchor_elements)
+    urls = set()
+    for anchor in anchor_elements:
+        url = anchor.get_attribute('href')
+        urls.add(url.strip())
+    return urls
