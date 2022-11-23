b8f7066274c6220582c24f8ee97d895ff2efe530
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-23 16:52:23 +0100

    add driver options

--- driver.py
+++ driver.py
@@ -5,4 +5,11 @@ from selenium.webdriver.chrome.service import Service
 service = Service(f'{os.getcwd()}/chromedriver')
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
+options.add_argument('incognito')
+options.add_argument('disable-infobars')
+options.page_load_strategy = 'eager'
+options.add_experimental_option(
+    # disable image loading
+    "prefs", {"profile.managed_default_content_settings.images": 2}
+)
 driver = webdriver.Chrome(service=service, options=options)
