9d22b9c0d7ae646ad0d8431f58ab3197974b3d2e
JohnEf <eustathiou.ioan@protonmail.com>
2022-11-24 21:49:53 +0100

    remove --disable-dev-shm-usage

--- driver.py
+++ driver.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.service import Service
 service = Service(f'{os.getcwd()}/chromedriver')
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
-options.add_argument('--disable-dev-shm-usage')
 options.add_argument('disable-infobars')
 options.page_load_strategy = 'eager'
 options.add_experimental_option(
